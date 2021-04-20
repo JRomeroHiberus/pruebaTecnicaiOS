5bf37df56ec3fc7c87d4337e689f6366d2345c31
Jose Romero <81155311+JRomeroHiberus@users.noreply.github.com>
2021-04-20 13:11:47 +0200

    ALMACENA Y MUESTRA

--- magic/magic/ItemViewController.swift
+++ magic/magic/ItemViewController.swift
@@ -21,8 +21,35 @@ class ItemViewController: UITableViewController {
         }
     }
     
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section:Int) -> Int {
+        //print (" Hay \(itemData.almacenItems.count) cartas en itemData")
+            print("ahora muestro")
+             return itemData.almacenItems.count
+            
+
+    }
+    
+    override func tableView(_ tableView:UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell{
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ItemCell", for: indexPath) as! ItemCell
+        
+        let item = itemData.almacenItems[indexPath.row]
+        cell.TextoCell.text = item.name
+        
+        return cell
+    }
+    
+  
+    
+    override func viewDidLoad() {
+        
+        super.viewDidLoad()
+        
+        tableView.rowHeight = UITableView.automaticDimension
+        tableView.estimatedRowHeight = 65
+        
+        
+        
         itemData = ItemData()
         cartaStore = CartaStore()
        /* for _ in 0...5 {
@@ -39,75 +66,25 @@ class ItemViewController: UITableViewController {
                 print("fetch correcto: encontradas \(items.count) cartas")
                 for i in items {
                     self.itemData.addItem(item: i)
+                    print("Ahora almaceno")
                    // print (" Hay \(self.itemData.almacenItems.count) cartas en itemData")
-
                 }
+                self.refresh()
             case let .failure(error):
                 print("error haciendo el fetch 1 \(error)")
             }
         }
+    
         
     }
     
-    
-    
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section:Int) -> Int {
-        print (" Hay \(itemData.almacenItems.count) cartas en itemData")
-        return itemData.almacenItems.count
+    func refresh(){
+        DispatchQueue.main.async(
+            execute:
+            {
+                print("Ejecuto refresh")
+                self.tableView.reloadData()
+            })
     }
-    
-    override func tableView(_ tableView:UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell{
-        let cell = tableView.dequeueReusableCell(withIdentifier: "ItemCell", for: indexPath) as! ItemCell
-        
-        let item = itemData.almacenItems[indexPath.row]
-        cell.TextoCell.text = item.name
-        
-        return cell
-    }
-    
-  
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        tableView.rowHeight = UITableView.automaticDimension
-        tableView.estimatedRowHeight = 65
-    
-      //  get_data("https://api.magicthegathering.io/v1/cards")
-       // cartaStore = CartaStore()
-       /* for _ in 0...5 {
-            itemData.crearItem()
-        }
-        for i in 0...5 {
-            print(itemData.almacenItems[i])
-        }*/
-        //cartaStore.fetchCards()
-       /* cartaStore.fetchCards{
-            (cardsResult) in
-            switch cardsResult{
-            case let .success(items):
-                print("fetch correcto: encontradas \(items.count) cartas")
-                for i in items {
-                    self.itemData.addItem(item: i)
-                    print (" Hay \(self.itemData.almacenItems.count) cartas en itemData")
-                }
-            case let .failure(error):
-                print("error haciendo el fetch 1 \(error)")
-            }
-        }*/
-        
-    }
-    
-    /*func updateImageView(for carta: Carta){
-        cartaStore.fetchImagen(for: carta) {
-            (imageResult) in
-            
-            switch imageResult {
-            case let .success(image) :
-                self.image
-            
-            }
-        }*/
 
-    
 }
